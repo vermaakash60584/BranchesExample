5b4b2e0b0c05ac25f315147258fd8cabbcbcfd8e
Akash Verma <vermaakash60584@gmail.com>
2022-07-07 14:54:28 +0530

    created a view

---
+++ Controllers/Branch.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BranchExercise.Controllers
+{
+    public class Branch : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
